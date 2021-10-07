a21039bbe825b97921c28c24c1af53c30980bd1d
preenabadjatia <89979051+preenabadjatia@users.noreply.github.com>
2021-10-07 16:45:12 +0530

    Update settings.py

--- ESHOP/settings.py
+++ ESHOP/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/download/"
 MEDIA_ROOT = BASE_DIR
+STATIC_ROOT= os.path.join(BASE_DIR,"static")
